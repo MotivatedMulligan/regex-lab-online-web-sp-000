9a40d307a866dca1fec750cc12517cd20e7d71cf
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-21 16:07:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+word = word.match(/\b[AEIOUaeiou]+\w*\b/) #fail
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text = text.scan(/\<(un)\w*(ing)\>/) #fail
 end
 
 def words_five_letters_long(text)
-
+text = text.scan(/\b\w{5}\b/) #this works!
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text = text.scan(/^[A-Z].*[[:punct:]]$/) #fail
 end
 
 def valid_phone_number?(phone)
-
+phone = phone.match(/\d{3}\W?\d{3}\W?\d{4}/) #this works!
 end
