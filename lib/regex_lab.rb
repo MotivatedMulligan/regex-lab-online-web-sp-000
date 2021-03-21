35e21dcd4965a34f430c19e893557378adb79a8b
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-21 16:22:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text = text.scan(/\<(un)\w*(ing)\>/) #fail
+/\<(un)\w*(ing)\>/ === text #fail
 end
 
 def words_five_letters_long(text)
@@ -12,7 +12,7 @@ text = text.scan(/\b\w{5}\b/) #this works!
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text = text.scan(/^[A-Z].*[[:punct:]]$/) #fail
+/^[A-Z].*[[:punct:]]$/ === text #fail
 end
 
 def valid_phone_number?(number)
