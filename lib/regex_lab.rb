f75a8f3427e8faa5ba375dabfdddb176dfbff6b8
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-21 16:18:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
-word = word.match(/\b[AEIOUaeiou]+\w*\b/) #fail
+#word = word.match(/\b[AEIOUaeiou]+\w*\b/)
+word = /\b[AEIOUaeiou]+\w*\b/.match(word) #fail
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -14,6 +15,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 text = text.scan(/^[A-Z].*[[:punct:]]$/) #fail
 end
 
-def valid_phone_number?(phone)
-phone = phone.match(/\d{3}\W?\d{3}\W?\d{4}/) #this works!
+def valid_phone_number?(number)
+number = number.match(/\d{3}\W?\d{3}\W?\d{4}/) #this works!
 end
